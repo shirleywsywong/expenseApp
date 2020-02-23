ab6e3785fa273f6245c5038490a3cf5e190ff13a
Shirley Wong <swong@theparcelport.com>
2020-02-22 21:08:10 -0500

    added clickable icons

--- components/expenseList.js
+++ components/expenseList.js
@@ -5,16 +5,15 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
-  Button,
-  TextInput,
-  Image,
+  TouchableOpacity,
 } from 'react-native';
 import RadioForm, {
   RadioButton,
   RadioButtonInput,
   RadioButtonLabel,
 } from 'react-native-simple-radio-button';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 
 class ExpenseList extends Component {
   constructor({navigation}) {
@@ -29,37 +28,39 @@ class ExpenseList extends Component {
     {label: 'param2', value: 1, amount: 18.67},
   ];
 
+  iconAdd = (<IconFeather name="plus-square" size={50} color="#109" />);
+  iconEdit = (<IconFeather name="edit" size={45} color="#109" />);
+  iconTrash = (<IconFeather name="trash-2" size={45} color="#109" />);
+  iconExport = (<IconMaterial name="export" size={50} color="#109" />);
+
   render() {
     return (
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <View style={styles.spacing}>
-            <View style={styles.spacing}>
+            <View style={styles.iconGroup}>
               <TouchableOpacity
-                onPress={this.props.navigation.navigate('ExpenseItem')}>
-                {/* {iconAdd} */}
-                <Text>+</Text>
+                style={styles.spacing}
+                onPress={() => this.props.navigation.navigate('ExpenseItem')}>
+                <IconFeather>{this.iconAdd}</IconFeather>
               </TouchableOpacity>
-            </View>
-            <View style={styles.spacing}>
-              <Button
-                title="Edit item"
-                onPress={() => this.props.navigation.navigate('ExpenseItem')}
-              />
-            </View>
-            <View style={styles.spacing}>
-              <Button
-                title="Delete item"
+              <TouchableOpacity
+                style={styles.spacing}
+                onPress={() => this.props.navigation.navigate('ExpenseItem')}>
+                <IconFeather>{this.iconEdit}</IconFeather>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.spacing}
                 onPress={() => {
                   alert('Are you sure?');
-                }}
-              />
-            </View>
-            <View style={styles.spacing}>
-              <Button
-                title="Export selected"
-                onPress={() => this.props.navigation.navigate('ExportCSV')}
-              />
+                }}>
+                <IconFeather>{this.iconTrash}</IconFeather>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.spacing}
+                onPress={() => this.props.navigation.navigate('ExportCSV')}>
+                <IconMaterial>{this.iconExport}</IconMaterial>
+              </TouchableOpacity>
             </View>
             <Text>Add an expense item</Text>
             <View style={styles.checkboxGroup}>
@@ -80,14 +81,15 @@ class ExpenseList extends Component {
 
 const styles = StyleSheet.create({
   spacing: {
-    margin: 20,
+    margin: 10,
   },
   TextInput: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
   },
-  checkboxGroup: {
+  iconGroup: {
+    margin: 20,
     flexDirection: 'row',
     padding: 10,
   },

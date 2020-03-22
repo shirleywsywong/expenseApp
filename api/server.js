1fe70f8ad967d66653a33b009c3e3d6ba3ab47b7
Shirley Wong <swong@theparcelport.com>
2020-03-22 14:18:23 -0400

    deployment file changes

--- api/server.js
+++ api/server.js
@@ -5,6 +5,9 @@ const userRouter = require("./routes/user/userRoutes");
 const expenseRouter = require("./routes/expense/expenseRoutes");
 const app = express(); //app represents the whole app, only instantiate it once
 
+const PORT = process.env.PORT || 8000
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/expenseApp'
+
 //in case url has non alpha-numeric characters, it parses to url encode
 app.use(express.urlencoded({ extended: true }));
 //middleware to manage route modules, all the functions that is associated with '/login' will go to the loginServer module, instead of written here
@@ -14,13 +17,13 @@ app.use("/expense", expenseRouter);
 
 mongoose
   //mongodb is at port 27017
-  .connect("mongodb://localhost:27017/expenseApp", {
+  .connect(MONGODB_URI, {
     userNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => {
     //talk to express through port 8000
-    app.listen("8000", () => {
+    app.listen(PORT, () => {
       console.log("8000")
     })
   })
